16766a2ef9e67325b4c3f87ecbb40ac1558af560
Michael Voznesensky <voznesenskym@gmail.com>
2023-07-11 21:23:10 +0000

    [WIP] Migrate tuple(handle) -> handle
    ghstack-source-id: 693f1b63a7ebaf8227954a7c0c0bbaff172e36fe
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/104488
    
    [WIP] Migrate tuple(handle) -> handle
    
    [WIP] Migrate tuple(handle) -> handle
    
    [WIP] Migrate tuple(handle) -> handle
    
    [WIP] Migrate tuple(handle) -> handle
    
    [WIP] Migrate tuple(handle) -> handle
    
    [WIP] Migrate tuple(handle) -> handle
    
    [WIP] Migrate tuple(handle) -> handle
    
    [WIP] Migrate tuple(handle) -> handle
    
    wip
    
    wip
    
    [WIP] Migrate tuple(handle) -> handle
    
    ghstack-source-id: 693f1b63a7ebaf8227954a7c0c0bbaff172e36fe
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/104488
    
    [WIP] Migrate tuple(handle) -> handle
    
    [WIP] Migrate tuple(handle) -> handle
    
    [WIP] Migrate tuple(handle) -> handle
    
    [WIP] Migrate tuple(handle) -> handle
    
    [WIP] Migrate tuple(handle) -> handle
    
    [WIP] Migrate tuple(handle) -> handle
    
    [WIP] Migrate tuple(handle) -> handle
    
    [WIP] Migrate tuple(handle) -> handle
    
    wip
    
    wip
    
    wip
    
    wip
    
    Tests
    
    Tests
    
    Tests

--- torch/distributed/fsdp/fully_sharded_data_parallel.py
+++ torch/distributed/fsdp/fully_sharded_data_parallel.py
@@ -480,11 +480,11 @@ class FullyShardedDataParallel(nn.Module, _FSDPState):
     @property
     def _has_params(self) -> bool:
         """Returns whether this FSDP instance manages any parameters."""
-        return hasattr(self, "_handles") and len(self._handles) > 0
+        return hasattr(self, "_handle") and self._handle > 0
 
     @property
     def _flat_param(self) -> Optional[FlatParameter]:
-        return self._handles[0].flat_param if self._handles else None
+        return self._handle.flat_param if self._handle else None
 
     def __getattr__(self, name: str) -> Any:
         """Forward missing attributes to the wrapped module."""
@@ -789,19 +789,25 @@ class FullyShardedDataParallel(nn.Module, _FSDPState):
         ):
             args, kwargs = _root_pre_forward(self, self, args, kwargs)
             unused = None
-            unshard_fn = functools.partial(_pre_forward_unshard, self, self._handles)
-            reshard_fn = functools.partial(_post_forward_reshard, self, self._handles)
             args, kwargs = _pre_forward(
-                self, self._handles, unshard_fn, self._fsdp_wrapped_module, args, kwargs
+                self,
+                self._handle,
+                _pre_forward_unshard,
+                self._fsdp_wrapped_module,
+                args,
+                kwargs,
             )
-            for handle in self._handles:
+            handle = self._handle
+            if handle:
                 _p_assert(
                     handle.flat_param.device == self.compute_device,
                     "Expected `FlatParameter` to be on the compute device "
                     f"{self.compute_device} but got {handle.flat_param.device}",
                 )
             output = self._fsdp_wrapped_module(*args, **kwargs)
-            return _post_forward(self, self._handles, reshard_fn, self, unused, output)
+            return _post_forward(
+                self, self._handle, _post_forward_reshard, self, unused, output
+            )
 
     @staticmethod
     @contextlib.contextmanager
@@ -920,8 +926,9 @@ class FullyShardedDataParallel(nn.Module, _FSDPState):
 
     def named_buffers(
         self,
-        *args,
-        **kwargs,
+        prefix: str = "",
+        recurse: bool = True,
+        remove_duplicate: bool = True,
     ) -> Iterator[Tuple[str, torch.Tensor]]:
         """
         Overrides :meth:`named_buffers()` to intercept buffer names and
@@ -929,7 +936,9 @@ class FullyShardedDataParallel(nn.Module, _FSDPState):
         when inside the :meth:`summon_full_params` context manager.
         """
         should_clean_name = self.training_state == TrainingState.SUMMON_FULL_PARAMS
-        for buffer_name, buffer in super().named_buffers(*args, **kwargs):
+        for buffer_name, buffer in super().named_buffers(
+            prefix=prefix, recurse=recurse, remove_duplicate=remove_duplicate
+        ):
             if should_clean_name:
                 # Remove any instances of the FSDP-specific prefix; there can
                 # be multiple in the case of nested FSDP modules
